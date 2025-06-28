37eb50e138f4ecec4c803d095d1c2f3d17e7f8f8
RivaldoCM <rivaldocardoso2010@hotmail.com>
2025-06-28 10:09:43 -0300

    style: Realocado colunas da tabela para mitigar problema de scroll lateral

--- src/pages/logs/onu/style.ts
+++ src/pages/logs/onu/style.ts
@@ -30,3 +30,26 @@ export const FilterButtons = styled.div`
         margin: .3rem;
     }
 `
+
+export const TableInsideTable = styled.div`
+    margin: 1rem;
+
+    table {
+        width: 30%;
+        border-collapse: collapse;
+        background-color: #ffffff;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    thead { background-color: #e0e0e0; }
+
+    th, td {
+        padding: 12px 15px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    tbody tr:nth-child(even) { background-color: #f9f9f9; }
+    tbody tr:hover{ background-color: #f1f1f1; }
+    th { color: #333; font-weight: bold; }
+`
